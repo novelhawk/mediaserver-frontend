4cca20aad85bc583121be8232c9c9549539eaaf6
Hawk <italia195@outlook.it>
2020-06-17 19:50:38 +0200

    Switched to functional components

--- pages/anime/index.js
+++ pages/anime/index.js
@@ -6,11 +6,11 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import fetch from 'isomorphic-unfetch';
 import Link from 'next/link';
 
-const styles = {
+const useStyles = makeStyles({
     grid: {
         width: '90%',
         margin: 'auto',
@@ -26,42 +26,40 @@ const styles = {
     image: {
         paddingTop: '131%',
     },
-};
+});
 
-class AnimeList extends React.Component {
-    render() {
-        const { classes, errorCode, animes } = this.props;
+export default function AnimeList({ errorCode, animes }) {
+    const classes = useStyles();
 
-        if (errorCode) {
-            return <Error statusCode={errorCode} />;
-        }
-
-        return (
-            <div className="AnimeListing">
-                <Typography variant="h3" className={classes.title} gutterBottom>
-                    Available animes
-                </Typography>
-                <Grid container direction="row" justify="center" spacing={2} className={classes.grid}>
-                    {animes.map((anime) => {
-                        return (
-                            <Grid key={anime.id} item>
-                                <Card className={classes.card}>
-                                    <Link href={`/anime/${anime.shortName}`}>
-                                        <CardActionArea>
-                                            {anime.cover && <CardMedia image={anime.cover} className={classes.image} />}
-                                            <CardContent>
-                                                <Typography variant="h5">{anime.displayName}</Typography>
-                                            </CardContent>
-                                        </CardActionArea>
-                                    </Link>
-                                </Card>
-                            </Grid>
-                        );
-                    })}
-                </Grid>
-            </div>
-        );
+    if (errorCode) {
+        return <Error statusCode={errorCode} />;
     }
+
+    return (
+        <div className="AnimeListing">
+            <Typography variant="h3" className={classes.title} gutterBottom>
+                Available animes
+            </Typography>
+            <Grid container direction="row" justify="center" spacing={2} className={classes.grid}>
+                {animes.map((anime) => {
+                    return (
+                        <Grid key={anime.id} item>
+                            <Card className={classes.card}>
+                                <Link href={`/anime/${anime.shortName}`}>
+                                    <CardActionArea>
+                                        {anime.cover && <CardMedia image={anime.cover} className={classes.image} />}
+                                        <CardContent>
+                                            <Typography variant="h5">{anime.displayName}</Typography>
+                                        </CardContent>
+                                    </CardActionArea>
+                                </Link>
+                            </Card>
+                        </Grid>
+                    );
+                })}
+            </Grid>
+        </div>
+    );
 }
 
 export async function getServerSideProps() {
@@ -75,5 +73,3 @@ export async function getServerSideProps() {
         return { props: { errorCode: 500 } };
     }
 }
-
-export default withStyles(styles)(AnimeList);
